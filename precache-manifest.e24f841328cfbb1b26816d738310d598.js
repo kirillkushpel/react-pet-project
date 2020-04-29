37063f63573e69e2f075b4c0493474a6f643981b
Kirill Kushpel <kkl@bork.ru>
2020-04-29 11:12:49 +0300

    Updates

---
+++ precache-manifest.e24f841328cfbb1b26816d738310d598.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29b58e8c84c2d90ecc21",
+    "url": "/react-messenger/static/css/main.56d358d8.chunk.css"
+  },
+  {
+    "revision": "29b58e8c84c2d90ecc21",
+    "url": "/react-messenger/static/js/main.29b58e8c.chunk.js"
+  },
+  {
+    "revision": "1448b4882759f1ced632",
+    "url": "/react-messenger/static/js/1.1448b488.chunk.js"
+  },
+  {
+    "revision": "6e6ca735b58c884b65db",
+    "url": "/react-messenger/static/js/runtime~main.6e6ca735.js"
+  },
+  {
+    "revision": "fb8e910e86b7b821eff956eb522c92ea",
+    "url": "/react-messenger/index.html"
+  }
+];
